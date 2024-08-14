3935bc3b40216c1522ca211828b4270dc7383a34
sonsu <sonsumin00@gmail.com>
2024-08-14 19:09:26 +0900

    feat: first commit

---
+++ src/test/java/issue/gpt/GptApplicationTests.java
@@ -0,0 +1,13 @@
+package issue.gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
